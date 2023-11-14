3c96df6683e9355fba5b8f457902ec17a4ac8a68
Jan Maaß <git@antipost.de>
2023-11-14 10:38:35 +0100

    Add solution

--- src/App.js
+++ src/App.js
@@ -5,9 +5,15 @@ import Box from "./components/Box";
 export default function App() {
   return (
     <main className="flex-container">
-      <Box color="#007bff" />
-      <Box color="#fc3" />
-      <Box color="#ff3333" />
+      <Boxes />
     </main>
   );
+}
+
+function Boxes() {
+  return <>
+    <Box color="#007bff" />
+    <Box color="#fc3" />
+    <Box color="#ff3333" />
+  </>
 }
